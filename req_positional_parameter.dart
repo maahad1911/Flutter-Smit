b6e531402b4eb7b323cbca4152803075cb28422c
maahad1911 <maahad1911@gmail.com>
2025-07-20 20:11:19 +0500

    Renamed req_parameter.dart to req_positional_parameter.dart and updated code

---
+++ req_positional_parameter.dart
@@ -0,0 +1,25 @@
+void main() {
+  String result = studentData("Mahad", 9217, "A");
+  print(result);
+
+  marksheet("Mahad", 78);
+  marksheet("Abdullah", 48);
+}
+
+String studentData(String name, int rollNo, String section) {
+  print("Name : $name");
+  print("Rollno : $rollNo");
+  print("Section : $section");
+
+  return "Student data recorded.";
+}
+
+marksheet(String name, int percentage) {
+  if (percentage > 50) {
+    print(name);
+    print("Pass");
+  } else {
+    print(name);
+    print("Fail");
+  }
+}
