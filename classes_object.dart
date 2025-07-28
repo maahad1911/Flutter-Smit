346388e99068ad42bea5c3e4ac3d7f767e48df4a
maahad1911 <maahad1911@gmail.com>
2025-07-28 22:48:17 +0500

    add new classes and object file

---
+++ classes_object.dart
@@ -0,0 +1,20 @@
+// ignore_for_file: unused_local_variable
+
+void main() {
+  var obj = Human();
+  print(obj.age);
+  var name = obj.name;
+  print(name);
+}
+
+class Human {
+  String name = "Mahad";
+  String age = "19";
+  String Uni = "ILMA";
+
+  eat() {}
+
+  smell() {}
+
+  count() {}
+}
