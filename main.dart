4ae57553d99c045705080336985f5a8db9c590ba
maahad1911 <maahad1911@gmail.com>
2025-07-30 23:06:24 +0500

    add new constructor file and update classes object code

--- main.dart
+++ main.dart
@@ -1,17 +1,54 @@
 void main() {
-  // sumOfTwoNum();
+  var results = main2();
 
-  var studentNames = ["Mahad", "Aram", "Kalid", "Nabi", "Serifer"];
-  var a = studentNames.length;
-  print(a);
-  
-  studentNames.sort();  // Sorts the list in place
-  print(studentNames);  // Print the sorted list
+  int choice = 7; // 0 = Add, 1 = Subtract, 2 = Divide, 3 = Multiply
+
+  if (choice == 0) {
+    print("The Value of ${results[0]}");
+  } else if (choice == 1) {
+    print("The Value of ${results[1]}");
+  } else if (choice == 2) {
+    print("The Value of ${results[2]}");
+  } else if (choice == 3) {
+    print("The Value of ${results[3]}");
+  } else {
+    print(
+      "Something went wrong,Your giving value is $choice please enter a correct value",
+    );
+  }
 }
 
+dynamic main2() {
+  var obj = Methmatics();
 
-// sumOfTwoNum({sub1, sub2, sub3}) {
-  // num a = sub1 + sub2 + sub3;
-  // print(a);
-  // return a;
-// }
+  List<dynamic> result = [
+    "Addition : ${obj.add(num1: 10, num2: 20)}",
+    "Subtraction : ${obj.subtract(num1: 10, num2: 2)}",
+    "Division : ${obj.divide(10, 5)}",
+    "Multiplicaion : ${obj.multiply(6, 4)}",
+  ];
+  return result;
+}
+
+class Methmatics {
+  int add({int? num1, int? num2}) {
+    var abc = (num1 ?? 0) + (num2 ?? 0);
+    return abc;
+  }
+
+  int subtract({int? num1, int? num2}) {
+    var abc = (num1 ?? 0) - (num2 ?? 0);
+    return abc;
+  }
+
+  double divide(int num1, int num2) {
+    var abc = num1 / num2;
+    return abc;
+  }
+
+  // It is required parameter //
+  int multiply(int num1, int num2) {
+    var abc = num1 * num2;
+    return abc;
+  }
+}
