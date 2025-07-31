f4d8db784c2d4b5497e9c759a902a199751c829f
maahad1911 <maahad1911@gmail.com>
2025-07-31 23:33:15 +0500

    Add new folder and update code in 1st constructor

--- constructor1.dart
+++ constructor1.dart
@@ -1,3 +1,26 @@
-void main () {
-  
+void main() {
+  Student obj = Student();
+  //here is ging values to constructor//
+  obj.studentInfo('Mahad', 19);
+  obj.infoStatus();
+
+  Student obj1 = Student();
+  obj.studentInfo("Abdullah", 20);
+  obj.infoStatus();
+}
+
+class Student {
+  String? namee;
+  int? agee;
+
+  // This is constructor it will take value from parameter in main function and send it to whole class//
+  studentInfo(String name, int age) {
+    this.namee = name;
+    this.agee = age;
+  }
+
+  infoStatus() {
+    print("Name : $namee");
+    print("Age : $agee");
+  }
 }
